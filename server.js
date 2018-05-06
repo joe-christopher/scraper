24949e9bc70a59afe176082c0f7cad3c2681853a
Joe Christopher <joechristopher@icloud.com>
2018-05-06 17:37:35 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -32,12 +32,10 @@ mongoose.Promise = Promise;
 
 if (process.env.MONGODB_URI){
   mongoose.connect(process.env.MONGODB_URI)
-}else{
-  mongoose.connect("mongodb://localhost/scraper", {
-      // useMongoClient: true
-  })
 }
-
+else{
+  mongoose.connect("mongodb://localhost/scraper")
+}
 
 // Routes
 
